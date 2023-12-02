646525123318f832bbc1651c05778224cfe398b8
Dirk Zwager <proditaki@gmail.com>
2023-12-02 22:02:36 +0100

    Update solution.py
    2nd solution for 2

--- day2/solution.py
+++ day2/solution.py
@@ -42,6 +42,22 @@ def solve2():
             amounts[block[1]] = int(block[0])
       sum += amounts['red'] * amounts['blue'] * amounts['green']
     print(sum)
+    
+def solve2b(): # alternate solution :)
+    input_filename = 'input.txt'
+    input_file = os.path.join(os.getcwd(), input_filename)
+    sum = 0
+    fHandle = open(input_file, 'r')
+    lines = fHandle.readlines()
+    for line in lines:
+      reds = re.findall(r' (\d+) red', line)
+      reds = [int(red) for red in reds]
+      blues = re.findall(r' (\d+) blue', line)
+      blues = [int(blue) for blue in blues]
+      greens = re.findall(r' (\d+) green', line)
+      greens = [int(green) for green in greens]
+      sum+=max(reds)*max(blues)*max(greens)
+    print(sum)
 if __name__ == "__main__":
     solve()
     solve2()
