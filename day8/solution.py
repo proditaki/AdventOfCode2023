f58532ab389b1fa83268569941a1c024a54db839
Dirk Zwager <proditaki@gmail.com>
2023-12-08 12:01:52 +0100

    Update solution.py
    added solution 2

--- day8/solution.py
+++ day8/solution.py
@@ -1,5 +1,5 @@
 import os
-
+import math
 def getInput(filename):
     input_file = os.path.join(os.getcwd(), filename)
     fHandle = open(input_file, 'r')
@@ -32,8 +32,37 @@ def solve():
             answer += 1
 
 def solve2():
-    pass
-
+    nodeList = {}
+    input_lines = getInput('input8.txt')
+    instructions = input_lines[0].strip()
+    for line in input_lines[2:]:
+        line = line.strip()
+        name,LR = line.split(' = ')
+        leftNode, rightNode =  LR[1:-1].split(', ')
+     #   print(name, leftNode, rightNode)
+        nodeList[name] = {'L': leftNode, 'R': rightNode}
+        #print(nodeList)
+    A_Nodes = [node for node in nodeList.keys() if node.endswith('A')]
+    totalSteps = {}
+    for i in range(len(A_Nodes)):
+        totalSteps[i] = 0
+    
+    for i, node in enumerate(A_Nodes):       
+        curNode = nodeList[node]
+        #print(curNode)
+        found = False
+        answer = 1
+        while not found:
+            for instruction in instructions:
+                if curNode[instruction].endswith('Z'):
+                    found = True
+                    totalSteps[i] = answer
+                    break           
+                curNode = nodeList[curNode[instruction]]
+               
+                answer += 1
+    totAnswer = 1
+    print(f'fastest to all Zs : {math.lcm(*totalSteps.values())}')
 if __name__ == "__main__":  
     solve()
     solve2()
